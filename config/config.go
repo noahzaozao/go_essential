0c4d8754fb7ab6bf170850b7c9540fdac635a0f2
wzy(Noah Wang) <234082230@qq.com>
2019-02-26 16:58:53 +0800

    update

--- config/config.go
+++ config/config.go
@@ -1,33 +1,33 @@
 package config
 
 type OSSConfig struct {
-	ENDPOINT          string `json:"ENDPOINT"`
-	ACCESS_KEY_ID     string `json:"ACCESS_KEY_ID"`
-	ACCESS_KEY_SECRET string `json:"ACCESS_KEY_SECRET"`
-	BUCKET_NAME       string `json:"BUCKET_NAME"`
+	ENDPOINT          string `yaml:"ENDPOINT"`
+	ACCESS_KEY_ID     string `yaml:"ACCESS_KEY_ID"`
+	ACCESS_KEY_SECRET string `yaml:"ACCESS_KEY_SECRET"`
+	BUCKET_NAME       string `yaml:"BUCKET_NAME"`
 }
 
 type DatabaseConfig struct {
-	Type string `json:"type"`
-	Host string `json:"host"`
-	Port string `json:"port"`
-	Database string `json:"database"`
-	User string `json:"user"`
-	Password string `json:"password"`
+	Type string `yaml:"type"`
+	Host string `yaml:"host"`
+	Port string `yaml:"port"`
+	Database string `yaml:"database"`
+	User string `yaml:"user"`
+	Password string `yaml:"password"`
 }
 
 type CacheConfig struct {
-	Type string `json:"type"`
-	Host string `json:"host"`
-	Port string `json:"port"`
-	DB int `json:"db"`
-	Password string `json:"password"`
+	Type string `yaml:"type"`
+	Host string `yaml:"host"`
+	Port string `yaml:"port"`
+	DB int `yaml:"db"`
+	Password string `yaml:"password"`
 }
 
 type SettingConfig struct {
-	SECRET_KEY string `json:"SECRET_KEY"`
-	DEBUG string `json:"DEBUG"`
-	DEFAULT_CHARSET string `json:"DEFAULT_CHARSET"`
-	DATABASES []DatabaseConfig `json:"DATABASES"`
-	CACHES []CacheConfig `json:"CACHES"`
+	SECRET_KEY string `yaml:"SECRET_KEY"`
+	DEBUG string `yaml:"DEBUG"`
+	DEFAULT_CHARSET string `yaml:"DEFAULT_CHARSET"`
+	DATABASES []DatabaseConfig `yaml:"DATABASES"`
+	CACHES []CacheConfig `yaml:"CACHES"`
 }
